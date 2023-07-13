f1a85d4c9294e8a1f5943bdc9880d151d5d88e22
Shoaib <37783369+mskiani@users.noreply.github.com>
2023-07-13 21:08:21 +0500

    latest api fix

--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,7 @@ import axios from "axios";
 const token = localStorage.getItem("accessToken");
 
 const api = axios.create({
-  baseURL: 'http://2algarve.site:5000/', // Replace with your base URL
+  baseURL: 'http://2algarve.site:5000/api/', // Replace with your base URL
   timeout: 5000, // Request timeout in milliseconds (optional)
   headers: {
     'Content-Type': 'application/json', // Example of setting a default header
